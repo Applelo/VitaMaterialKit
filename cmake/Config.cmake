7d258b52d21e4c08da442fdf68cadc1a03e3e1d8
Loïs <boubaultlois@gmail.com>
2019-06-29 20:22:44 -0400

    Add config to set the psvita volume path for copy commands

--- cmake/Config.cmake
+++ cmake/Config.cmake
@@ -3,7 +3,9 @@ set(VITA_APP_NAME "VitaMaterialKit")
 set(VITA_TITLEID "MKIT00001")
 set(VITA_VERSION "00.75")
 
-set(PSVITAIP "192.168.0.192")
+#Vita send
+set(PSVITA_IP "192.168.0.192")
+set(PSVITA_VOLUME "/Volumes/Untitled/")
 
 #Debug
 set(DEBUG_IP "192.168.0.108")
